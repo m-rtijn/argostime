7b34fa16edc35a077e67ac7951e2d5c3cafb5355
Martijn <martijn@mrtijn.nl>
2022-02-05 17:11:54 +0100

    Add random delays in price update script

--- argostime_update_prices.py
+++ argostime_update_prices.py
@@ -7,11 +7,14 @@
     Standalone script to update prices in the database.
 """
 
+import random
 import logging
 import time
 
 from argostime.models import ProductOffer
 from argostime import create_app
+
+time.sleep(random.uniform(0, 600))
 
 app = create_app()
 app.app_context().push()
@@ -20,4 +23,4 @@ offer: ProductOffer
 for offer in ProductOffer.query.all():
     logging.info("Crawling %s", str(offer))
     offer.crawl_new_price()
-    time.sleep(1)
+    time.sleep(1 + random.uniform(1, 60))
