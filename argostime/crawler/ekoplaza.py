551f8d85f9b5807eae13ed0b6dc4314938793fbb
Anna <annaproxy@protonmail.com>
2022-05-12 13:37:35 +0200

    update header for ekoplaza (add license & remove imports)

--- argostime/crawler/ekoplaza.py
+++ argostime/crawler/ekoplaza.py
@@ -1,11 +1,34 @@
-from datetime import date
+#!/usr/bin/env python3
+"""
+    crawler/ekoplaza.py
+
+    Crawler for ekoplaza.nl
+
+    Copyright (c) 2022 Anna <anna [at] anna.computer>
+
+    This file is part of Argostimè.
+
+    Argostimè is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Argostimè is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Argostimè. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
 import json
 import logging
 from pprint import pprint
 
 
 import requests
-from bs4 import BeautifulSoup
 
 from argostime.exceptions import CrawlerException
 from argostime.exceptions import PageNotFoundException
