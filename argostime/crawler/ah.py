21322c691c20153a2638ce35683b7bfbd451d019
defterade <de@ftera.de>
2022-05-15 16:43:19 +0200

    AH crawler: use API, fix discount price bug

--- argostime/crawler/ah.py
+++ argostime/crawler/ah.py
@@ -23,139 +23,111 @@
 """
 
 from datetime import date
-import json
 import logging
-
-
+import re
 import requests
-from bs4 import BeautifulSoup
 
 from argostime.exceptions import CrawlerException
 from argostime.exceptions import PageNotFoundException
 
-from argostime.crawler.crawl_utils import CrawlResult, parse_promotional_message
+from argostime.crawler.crawl_utils import CrawlResult
 
 def crawl_ah(url: str) -> CrawlResult:
     """Crawler for ah.nl"""
-    response: requests.Response = requests.get(url)
+
+    product_id_re = re.search(r"\/wi(\d+)\/", url)
+    if not product_id_re:
+        logging.error("No product ID found in URL %s", url)
+        raise PageNotFoundException(url)
+
+    product_id = product_id_re.group(1)
+
+    response = requests.get(f"https://www.ah.nl/zoeken/api/products/product?webshopId={product_id}")
 
     if response.status_code != 200:
         logging.error("Got status code %d while getting url %s", response.status_code, url)
         raise PageNotFoundException(url)
 
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    raw_json_match = soup.find(
-        "script",
-        attrs={ "type": "application/ld+json", "data-react-helmet": "true"}
-        )
+    try:
+        product = response.json()['card']['products'][0]
+    except Exception as exception:
+        logging.error("Could not obtain product data for product id %s", product_id)
+        raise CrawlerException from exception
 
     result: CrawlResult = CrawlResult(url=url)
 
     try:
-        product_dict = json.loads(raw_json_match.text)
-    except json.decoder.JSONDecodeError as exception:
-        logging.error("Could not decode JSON %s, raising CrawlerException", raw_json_match)
-        raise CrawlerException from exception
-    except Exception as exception:
-        logging.error(
-            "Could not find a JSON to parse? Got Exception %s parsing %s",
-            exception,
-            raw_json_match)
-        raise CrawlerException from exception
-
-    try:
-        result.product_name = product_dict["name"]
+        result.product_name = product["title"]
     except KeyError as exception:
-        logging.error("No key name found in json %s", product_dict)
+        logging.error("No key title found in json %s", product)
         raise CrawlerException from exception
 
     try:
-        result.ean = product_dict["gtin13"]
+        result.ean = product["gtins"][0]
     except KeyError:
         # Don't crash because the ean is not strictly necessarry
-        logging.info("No key gtin13 found in json %s", product_dict)
+        logging.info("No key gtins found in json %s", product)
+    except IndexError:
+        logging.info("No ean found in json %s", product)
 
     try:
-        result.product_code = product_dict["sku"]
+        result.product_code = f"wi{product['id']}"
     except KeyError as exception:
-        logging.error("No key sku found in json %s", product_dict)
+        logging.error("No key id found in json %s", product)
         raise CrawlerException from exception
 
     try:
-        offer = product_dict["offers"]
+        price = product["price"]
     except KeyError as exception:
-        logging.error("Could not find a valid offer in the json %s", product_dict)
+        logging.error("No key price found in json %s", product)
         raise CrawlerException from exception
 
-    if "validFrom" in offer.keys():
+    if price is not dict:
+        logging.error("No valid price found for %s", url)
+
+    try:
+        price_now : float = float(price["now"])
+    except KeyError as exception:
+        logging.error("Could not find a price for url %s", url)
+        raise CrawlerException from exception
+
+    if "discount" in product:
+        discount = product['discount']
+
+        bonus_from : date = date(year=2000, month=1, day=1)
         try:
-            bonus_from: date = date.fromisoformat(offer["validFrom"])
+            bonus_from = date.fromisoformat(discount['startDate'])
+        except KeyError:
+            logging.error("No key startDate found in dict %s", discount)
         except ValueError:
             logging.error(
-                "Failed to parse validFrom %s, assuming bonus is valid",
-                offer["validFrom"]
+                "Failed to parse startDate %s, assuming bonus is valid",
+                discount['startDate']
                 )
-            bonus_from = date(year=2000, month=1, day=1)
+
+        bonus_until : date = date(year=5000, month=12, day=31)
         try:
-            bonus_until: date = date.fromisoformat(offer["priceValidUntil"])
+            bonus_until = date.fromisoformat(discount['endDate'])
+        except KeyError:
+            logging.error("No key endDate found in dict %s", discount)
         except ValueError:
             logging.error(
                 "Failed to parse priceValidUntil %s, using fallback",
-                offer["priceValidUntil"]
-                )
-            bonus_until = date(year=5000, month=12, day=31)
-
-        if date.today() >= bonus_from and date.today() <= bonus_until:
-            # Try to find a promotional message
-            promo_text_matches = soup.find_all(
-                "p",
-                attrs={ "class" :lambda x: x and x.startswith("promo-sticker-text") }
-                )
-            
-            if len(promo_text_matches) == 0:
-                promo_text_matches = soup.find_all(
-                    "div",
-                    attrs={ "class" :lambda x: x and x.startswith("promo-sticker_content") }
-                    )
-
-            promotion_message: str = ""
-            for match in promo_text_matches:
-                promotion_message = promotion_message + match.text
-
-            # Remove all whitespace from the message
-            message_no_whitespace = "".join(promotion_message.split())
-            message_no_whitespace.lower()
-
-            price: float = float(offer["price"])
-
-            # If there is a mark with for example "25% Korting", this is already calculated into
-            # the price we got from the json.
-            if "korting" not in message_no_whitespace:
-                promotion = parse_promotional_message(promotion_message, price)
-            else:
-                promotion = -1
-
-            logging.debug(
-                "Found promotional message %s with result %f",
-                promotion_message,
-                promotion
+                discount["endDate"]
                 )
 
-            if promotion != -1:
-                result.discount_price = promotion
-            else:
-                result.discount_price = price
+        if bonus_from <= date.today() <= bonus_until:
+            result.discount_price = price_now
         else:
-            # No valid bonus, so there's no valid price available.
-            logging.info("No valid price found for %s", url)
-            result.normal_price = -1
+            # "now" price includes discount that isn't active
+            try:
+                price_was: float = float(price["was"])
+            except KeyError:
+                logging.error("Could not find old price for product %s, using discounted price", url)
+                price_was = price_now
+
+            result.normal_price = price_was
     else:
-        # No details on if there is bonus data or not, so assume no bonus
-        try:
-            result.normal_price = float(product_dict["offers"]["price"])
-        except KeyError as inner_exception:
-            logging.error("Couldn't even find a normal price in %s", product_dict)
-            raise CrawlerException from inner_exception
+        result.normal_price = price_now
 
     return result
