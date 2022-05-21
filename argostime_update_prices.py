769de2880c4fc36833f062632d3451957552c194
Martijn <martijn@mrtijn.nl>
2022-05-21 21:17:39 +0200

    Correct file headers to specify that the Affero GPLv3 is used

--- argostime_update_prices.py
+++ argostime_update_prices.py
@@ -2,9 +2,24 @@
 """
     argostime_update_prices.py
 
-    Part of Argostimè
-
     Standalone script to update prices in the database.
+
+    Copyright (c) 2022 Martijn <martijn [at] mrtijn.nl>
+
+    This file is part of Argostimè.
+
+    Argostimè is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Argostimè is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with Argostimè. If not, see <https://www.gnu.org/licenses/>.
 """
 
 import random
